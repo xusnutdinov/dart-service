15cad2c55f8e66688f2910e616e249a38930a1c9
xusnutdinov.rin@gmail.com <xusnutdinov.rin@gmail.com>
2021-02-06 19:49:54 +0300

    desktop vesrion

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 const servicesItems = document.querySelectorAll('.services__top');
 
-servicesItems.forEach( function(item) {
-	item.addEventListener('click', function(event) {
+servicesItems.forEach(function (item) {
+	item.addEventListener('click', function (event) {
 		let servicesBody = item.closest('.services__item').querySelector('.services__body');
 
 		if (servicesBody.classList.contains('active')) {
 			servicesBody.classList.remove('active');
 		} else {
-			document.querySelectorAll('.services__body').forEach( function(element) {
+			document.querySelectorAll('.services__body').forEach(function (element) {
 				element.classList.remove('active');
 			});
 			servicesBody.classList.add('active');
@@ -27,7 +27,40 @@ const testimonials = new Swiper('.testimonials-slider__container', {
 		disableOnInteraction: true
 	},
 	navigation: {
-	    nextEl: '.testimonials-slider-next',
-	    prevEl: '.testimonials-slider-prev',
-	  },
+		nextEl: '.testimonials-slider-next',
+		prevEl: '.testimonials-slider-prev',
+	},
+});
+
+
+
+
+
+const photoSlider = new Swiper('.photo-slider', {
+	loop: true,
+	slidesPerView: 3,
+	centeredSlides: true,
+	slideToClickedSlide: true,
+	loopedSlides: 5,
+	allowTouchMove: false,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: true
+	},
+});
+
+const textSlider = new Swiper('.text-slider', {
+	loop: true,
+	slidesPerView: 1,
+	centeredSlides: true,
+	loopedSlides: 5,
+	autoHeight: true,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: true
+	},
+	thumbs: {
+		swiper: photoSlider,
+		multipleActiveThumbs: true,
+	},
 });
